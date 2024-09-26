f24fcc43a7bc8a1e4d406e3060bd21fc2485b37c
Guillermo Marcos <extern.guillermo.marcos>
2024-09-26 14:00:14 +0200

    creacion de páginas y componentes inciales, enrutador funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch:"full", redirectTo: "home"},
+    {path: "home", component: HomeComponent},
+    {path: "sign-in", component: SignInComponent},
+    {path: "sign-up", component: SignUpComponent}   
+];
