c948ed77295e46f71ed52deda7f540676cbc4146
Guillermo Marcos <extern.guillermo.marcos>
2024-10-02 16:03:27 +0200

    maquetación y usabilidad de los botones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MapComponent } from '../../components/map/map.component';
 import { ProfilePreviewComponent } from '../../components/profile-preview/profile-preview.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapComponent,ProfilePreviewComponent],
+  imports: [MapComponent,ProfilePreviewComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
