9fdd4383c8c8458f13313fc49514e3801a076fa6
Guillermo Marcos <extern.guillermo.marcos>
2024-10-03 18:17:39 +0200

    formularios enlazados, sin validaciones y campos faltantes

--- src/app/components/forms/teacher-form/teacher-form.component.ts
+++ src/app/components/forms/teacher-form/teacher-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-teacher-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './teacher-form.component.html',
   styleUrl: './teacher-form.component.css'
 })
 export class TeacherFormComponent {
+    modelForm:  FormGroup;
 
+    constructor(){
+      this.modelForm = new FormGroup({
+        name: new FormControl(null, [
+          Validators.required
+        ]),
+        surname: new FormControl(null, [
+          Validators.required
+        ])
+        
+
+      })
+    }
+
+    signUp(){
+
+    }
 }
