efd51756c910a90ed257be4051215d0236d1b64c
jpecina <jpecina@gmail.com>
2024-10-17 16:38:42 +0200

    Forbidden Page
    Esta es la página que se muestra cuando el usuario intenta acceder a una página no autorizada.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { TeachersComponent } from './pages/teachers/teachers.component';
 import { ProfileComponent } from './pages/profile/profile.component';
-import { NotFoundError } from 'rxjs';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ForbiddenComponent } from './pages/forbidden/forbidden.component';
 
 export const routes: Routes = [
     {path: "", pathMatch:"full", redirectTo: "home"},
@@ -14,5 +14,6 @@ export const routes: Routes = [
     {path: "sign-up", component: SignUpComponent},
     {path: "teachers", component: TeachersComponent},      
     {path: "profile", component: ProfileComponent},
-    {path: "not-found", component: NotFoundComponent}
+    {path: "not-found", component: NotFoundComponent},
+    {path: "forbidden", component: ForbiddenComponent}
 ];
