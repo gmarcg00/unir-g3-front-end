ab1c3f56cc4dfe80ec1a60a480b03d77dcda3287
jpecina <jpecina@gmail.com>
2024-10-26 11:41:47 +0200

    Organización carpeta imagenes
    He creado la carpeta public/images y luego he ordenado las fotos

--- src/app/pages/testimonios/testimonios.component.ts
+++ src/app/pages/testimonios/testimonios.component.ts
@@ -13,23 +13,28 @@ export class TestimoniosComponent {
   testimonios = [
     {
       nombre: 'Carlos Martínez',
-      mensaje: 'La plataforma me ha permitido encontrar al mejor profesor para mejorar mi inglés rápidamente. Muy intuitiva y fácil de usar.'
+      mensaje: 'La plataforma me ha permitido encontrar al mejor profesor para mejorar mi inglés rápidamente. Muy intuitiva y fácil de usar.',
+      ruta: '/images/estudiantes/fotor-ai-2024102610534.jpg'
     },
     {
       nombre: 'Ana García',
-      mensaje: 'Gracias a esta página he podido aprender matemáticas con un profesor excelente que vive cerca de mí.'
+      mensaje: 'Gracias a esta página he podido aprender matemáticas con un profesor excelente que vive cerca de mí.',
+      ruta: '/images/estudiantes/fotor-ai-20241026105154.jpg'
     },
     {
       nombre: 'Luis Fernández',
-      mensaje: 'Lo que más me gusta es el sistema de valoración de profesores, me da mucha confianza para elegir al mejor.'
+      mensaje: 'Lo que más me gusta es el sistema de valoración de profesores, me da mucha confianza para elegir al mejor.',
+      ruta: '/images/estudiantes/fotor-ai-20241026105215.jpg'
     },
     {
       nombre: 'María López',
-      mensaje: 'He contactado con varios profesores por el chat y la experiencia ha sido muy fluida. Muy recomendable.'
+      mensaje: 'He contactado con varios profesores por el chat y la experiencia ha sido muy fluida. Muy recomendable.',
+      ruta: '/images/estudiantes/fotor-ai-20241026105255.jpg'
     },
     {
       nombre: 'Jorge Pérez',
-      mensaje: 'Pude elegir un profesor particular que se ajustaba a mi horario y presupuesto. La plataforma es genial.'
+      mensaje: 'Pude elegir un profesor particular que se ajustaba a mi horario y presupuesto. La plataforma es genial.',
+      ruta: '/images/estudiantes/fotor-ai-20241026105255.jpg'
     }
   ];
 }
