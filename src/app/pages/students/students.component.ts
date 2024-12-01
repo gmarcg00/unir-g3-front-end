a4cf29ada312d9a1cdc77b4b9976a9135d09ac83
Guillermo Marcos <extern.guillermo.marcos>
2024-12-02 00:12:15 +0100

    finalizado a falta del boton de perfil en el card del alumno

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -2,11 +2,16 @@ import {Component, inject} from '@angular/core';
 import {StudentsService} from "../../services/students.service";
 import {IStudentInfoInterface} from "../../interfaces/iStudentInfo.interface";
 import {IListResponseInterface} from "../../interfaces/iListResponse.interface";
+import {NgForOf} from "@angular/common";
+import {StudentCardComponent} from "../../components/student-card/student-card.component";
 
 @Component({
   selector: 'app-students',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    StudentCardComponent
+  ],
   templateUrl: './students.component.html',
   styleUrl: './students.component.css'
 })
@@ -18,6 +23,24 @@ export class StudentsComponent {
   pageSize: number = 12;
   totalPages: number = 0;
   pages: number[] = [];
+
+  ngOnInit(){
+    this.getData().then(() => {
+      this.updatePages()
+    });
+  }
+
+  updatePages(): void {
+    this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number): void {
+    if (page < 1 || page > this.totalPages || page === this.currentPage) {
+      return;
+    }
+    this.currentPage = page;
+    this.getData();
+  }
 
   async getData(): Promise<void> {
     const response: IListResponseInterface = await this.studentsService.getAll(this.currentPage,this.pageSize)
