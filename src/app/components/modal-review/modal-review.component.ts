c39d27cef07173edb91d294f522dc9d74fd905aa
Gabriel Martí Capell <electro.gnu@gmail.com>
2024-12-05 19:20:18 +0000

    modal_review starting

---
+++ src/app/components/modal-review/modal-review.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-review',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-review.component.html',
+  styleUrl: './modal-review.component.css'
+})
+export class ModalReviewComponent {
+
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MapComponent } from '../../components/map/map.component';
 import { RouterLink } from '@angular/router';
-import {TeachersService} from "../../services/teachers.service";
-import {ITeacherInfoInterface} from "../../interfaces/iTeacherInfoInterface";
+import { TeachersService } from "../../services/teachers.service";
+import { ITeacherInfoInterface } from "../../interfaces/iTeacherInfoInterface";
 import Swal from 'sweetalert2';
-import {TeacherCardComponent} from "../../components/teacher-card/teacher-card.component";
-import {NgForOf} from "@angular/common";
+import { TeacherCardComponent } from "../../components/teacher-card/teacher-card.component";
+import { NgForOf } from "@angular/common";
+import { ModalReviewComponent } from '../../components/modal-review/modal-review.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MapComponent, RouterLink, TeacherCardComponent, NgForOf],
+  imports: [MapComponent, RouterLink, TeacherCardComponent, NgForOf, ModalReviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -20,14 +21,14 @@ export class HomeComponent {
   teachers: ITeacherInfoInterface[] = [];
   distance: number = 3;
 
-  async ngOnInit(){
-    this.teachersService.getBestAverageRatingTeachers(1,4)
+  async ngOnInit() {
+    this.teachersService.getBestAverageRatingTeachers(1, 4)
       .then(response => this.teachers = response.data)
-      .catch(() => Swal.fire("Error","An error occurred while fetching the data","error"));
+      .catch(() => Swal.fire("Error", "An error occurred while fetching the data", "error"));
 
   }
 
-  selectDistance(distance: number){
+  selectDistance(distance: number) {
     this.distance = distance;
     console.log(this.distance);
   }
