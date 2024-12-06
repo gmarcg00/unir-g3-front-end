bb7fa92285440c2eedef6c551a4f9e6941843dae
Lara Tatiana <laratatiana2024@gmail.com>
2024-12-06 02:07:57 +0100

    cambio orden landing, profes, mapa y pie de pagina

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,18 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MapComponent } from '../../components/map/map.component';
 import { RouterLink } from '@angular/router';
-import {TeachersService} from "../../services/teachers.service";
-import {ITeacherInfoInterface} from "../../interfaces/iTeacherInfoInterface";
+import { TeachersService } from "../../services/teachers.service";
+import { ITeacherInfoInterface } from "../../interfaces/iTeacherInfoInterface";
 import Swal from 'sweetalert2';
-import {TeacherCardComponent} from "../../components/teacher-card/teacher-card.component";
-import {NgForOf} from "@angular/common";
+import { TeacherCardComponent } from "../../components/teacher-card/teacher-card.component";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [MapComponent, RouterLink, TeacherCardComponent, NgForOf],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'] // Cambiado de styleUrl a styleUrls
 })
 export class HomeComponent {
 
@@ -20,17 +20,17 @@ export class HomeComponent {
   teachers: ITeacherInfoInterface[] = [];
   distance: number = 3;
 
-  async ngOnInit(){
-    this.teachersService.getBestAverageRatingTeachers(1,4)
-      .then(response => this.teachers = response.data)
-      .catch(() => Swal.fire("Error","An error occurred while fetching the data","error"));
-
+  async ngOnInit() {
+    try {
+      const response = await this.teachersService.getBestAverageRatingTeachers(1, 4);
+      this.teachers = response.data;
+    } catch (error) {
+      Swal.fire("Error", "An error occurred while fetching the data", "error");
+    }
   }
 
-  selectDistance(distance: number){
+  selectDistance(distance: number) {
     this.distance = distance;
     console.log(this.distance);
   }
-
-
 }
