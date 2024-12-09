3db9ef78d1623e5a71dc8bbb493c9e8bbe6d4715
Gabriel Martí Capell <electro.gnu@gmail.com>
2024-12-09 19:01:40 +0000

    esqueleto home-students

--- src/app/pages/home-student/home-student.component.ts
+++ src/app/pages/home-student/home-student.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { MapComponent } from '../../components/map/map.component';
 
 @Component({
   selector: 'app-home-student',
   standalone: true,
-  imports: [],
+  imports: [MapComponent],
   templateUrl: './home-student.component.html',
   styleUrl: './home-student.component.css'
 })
